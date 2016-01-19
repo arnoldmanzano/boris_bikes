0e4724d45abf79dc306a1c653896605e32929f0c
itsindigo <matthewbridgesbusiness@gmail.com>
2016-01-19 14:18:49 +0000

    Added Bike class file with working meth. added tests to ensure docking station was relasing a new, working bike.

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -8,4 +8,10 @@ require 'docking_station'
 
 describe DockingStation do
 	it { is_expected.to respond_to :release_bike}
+#	it { expect(DockingStation.new.release_bike.class).to eq Bike }
+	it { expect(subject.release_bike.class).to eq Bike }
+	it 'released working bikes' do
+		bike = subject.release_bike
+		expect(bike).to be_working
+	end
 end
