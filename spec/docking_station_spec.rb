efdba47db0cbdae908a5554a9e72d3cc93123d6b
itsindigo <matthewbridgesbusiness@gmail.com>
2016-01-19 13:09:40 +0000

    Added test for docking station to respond to release_bike plus release_bike method

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,11 @@
 require 'docking_station'
 
+#describe DockingStation do
+#	it "Class instance should respond to release_bike" do
+#		expect(subject).to respond_to :release_bike
+#	end	
+#end
+
 describe DockingStation do
-	
+	it { is_expected.to respond_to :release_bike}
 end
