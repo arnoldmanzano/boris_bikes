c19952346f393eb0c18369ec2e897ecdd77d0c4b
arnoldmanzano <arnold.manzano@gmail.com>
2016-01-19 16:23:11 +0000

    Added DockingStation dock and bike tests and methods and passed

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,9 +1,24 @@
 require_relative 'bike'
 
 class DockingStation
+#	attr_reader :bike_count
+  attr_reader :bike
+
+#	def initialize(bike_count=10)
+#		@bike_count = bike_count
+#	end
+
 	def release_bike
+#		@bike_count -= 1
 		Bike.new
 	end
+
+  def dock(bike)
+#		@bike_count += 1
+#		@bike_count
+		@bike = bike
+	end
+
 end
 
 docking_station = DockingStation.new
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,17 +1,32 @@
 require 'docking_station'
 
-#describe DockingStation do
-#	it "Class instance should respond to release_bike" do
-#		expect(subject).to respond_to :release_bike
-#	end	
-#end
-
 describe DockingStation do
 	it { is_expected.to respond_to :release_bike}
-#	it { expect(DockingStation.new.release_bike.class).to eq Bike }
 	it { expect(subject.release_bike.class).to eq Bike }
+
 	it 'released working bikes' do
 		bike = subject.release_bike
 		expect(bike).to be_working
 	end
+
+	it { is_expected.to respond_to(:dock).with(1).argument }
+
+  it 'docks bike to station' do
+		bike = Bike.new
+		expect(subject.dock(bike)).to eq bike
+  end
+
+	it 'shows docked bike with same id' do
+		bike = Bike.new
+		subject.dock(bike)
+		expect(subject.bike).to eq bike
+	end
+
+#	it { expect(subject.bike_count).to be <= 10 }
+#  it 'docks bike to station, increasing bike_count' do
+#		initial_bikes = subject.bike_count
+#		final_bikes = subject.dock_bike(Bike.new)
+#		expect(final_bikes).to be > initial_bikes
+#	end
+
 end
