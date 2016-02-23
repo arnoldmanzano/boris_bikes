ebdfc999b1e38d64d27ec82a5cbde62d5e78d27b
arnoldmanzano <arnold.manzano@gmail.com>
2016-02-23 11:08:32 +0000

    Added van spec and code

---
+++ lib/van.rb
@@ -0,0 +1,18 @@
+require_relative 'docking_station'
+
+class Van
+  attr_reader :bikes
+
+  def initialize
+    @bikes = []
+  end
+
+  def collect_from(place)
+    @bikes = place.release_bikes
+  end
+
+  def deliver_to(place)
+    place.receive_bikes(@bikes.pop(@bikes.size))
+  end
+
+end
